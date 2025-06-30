db47d36569e89c4759baeda7a32436b458001e6e
Kavindu Sandaruwan <87182670+ImKavinduSandaruwan@users.noreply.github.com>
2025-06-30 13:50:09 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -2,7 +2,7 @@
 $host = "gatepass-mysql.mysql.database.azure.com";
 $dbname = "gatepass_db";  // this should be your database name
 $username = "gatepassadmin@gatepass-mysql";       // default username for XAMPP/WAMP
-$password = "Shwh621$21";           // default password is empty
+$password = "Shwh621$21Sihsihqish";           // default password is empty
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
